492f498d839d8a7e84594165fd6f85ab99b20086
alanwsh <72345813+alanwsh@users.noreply.github.com>
2022-06-29 22:25:33 +0800

    format sign up structure

--- util/auth.js
+++ util/auth.js
@@ -1,12 +1,20 @@
 import axios from "axios";
 
 const API_KEY = 'AIzaSyAeJuDv6YksPZ_njlaZ4_rRW43DgDe71c8';
-const BASE_URL = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`;
+const BASE_URL = 'https://identitytoolkit.googleapis.com/v1/accounts:';
+
+function getRequestUrl(mode){
+    return `${BASE_URL}${mode}?key=${API_KEY}`;
+}
 
 export async function createUser(email, password){
-    const res = axios.post(BASE_URL, {
+    const res = axios.post(getRequestUrl('signUp'), {
         email: email,
         password: password,
         returnSecureToken: true
     });
+}
+
+export async function authenticate(mode, email, password){
+
 }
