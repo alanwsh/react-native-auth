2f7d6bfbec1fe3ea0901edd39ecafa2cdada250f
alanwsh <72345813+alanwsh@users.noreply.github.com>
2022-07-02 19:09:02 +0800

    use auth context to store login token

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,17 +1,20 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import AuthContent from '../components/Auth/AuthContent';
 import { createUser } from '../util/auth';
 import LoadingOverlay from '../components/ui/LoadingOverlay';
 import { Alert } from 'react-native';
+import { AuthContext } from '../store/auth-context';
 
 function SignupScreen() {
 
   const [isLoading, setLoading] = useState(false);
 
+  const authCtx = useContext(AuthContext);
   async function signUpHandler({email, password}){
     setLoading(true);
     try{
-      await createUser(email, password);
+      const token = await createUser(email, password);
+      authCtx.authenticate(token);
     }
     catch(err){
       Alert.alert('Authentication failed','Could not sign up the user');
--- util/auth.js
+++ util/auth.js
@@ -3,25 +3,25 @@ import axios from "axios";
 const API_KEY = 'AIzaSyAeJuDv6YksPZ_njlaZ4_rRW43DgDe71c8';
 const BASE_URL = 'https://identitytoolkit.googleapis.com/v1/accounts:';
 
-export async function createUser(email, password){
-    await authenticate('signUp',email,password);
+export function createUser(email, password){
+    return authenticate('signUp',email,password);
 }
 
-export async function login(email,password){
-    await authenticate('signInWithPassword',email,password);
+export  function login(email,password){
+    return authenticate('signInWithPassword',email,password);
 }
 
 export async function authenticate(mode, email, password){
     const url = `${BASE_URL}${mode}?key=${API_KEY}`;
-    console.log(url);
     const body ={
         email: email,
         password: password,
         returnSecureToken: true
     }
     try{
-        const res = axios.post(url,body); 
-        return res;
+        const res = await axios.post(url,body);
+        const token = res.data.idToken;
+        return token;
     }catch(err){
         return err;
     }
